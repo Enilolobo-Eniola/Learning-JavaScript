14d64be3e00161f629ca05653f4aa05aecd8f2a4
Enilolobo Eniola <eniloloboeniolaisrael@gmail.com>
2020-03-19 15:24:30 +0100

    new updates to design and funtionalities

--- practice1.js
+++ practice1.js
@@ -5,24 +5,100 @@
 // console.log(shit);
 // console.log(shit.length);
 
-var newItem = document.getElementById("newItem");
-var addBtn = document.getElementById("addBtn");
-var list1 = document.querySelector("ul");
-var errorString = "Ensure the field isn't empty";
+// var newItem = document.getElementById("newItem");
+// var addBtn = document.getElementById("addBtn");
+// var list1 = document.querySelector("ul");
+// var errorString = "Ensure the field isn't empty";
 
-function addToList()
+// function addToList()
+// {
+// 	if (newItem.value.length > 0 )
+//  	{
+//  		var li = document.createElement("li");
+//  		li.appendChild(document.createTextNode(newItem.value));
+//  		list1.appendChild(li);
+//  		newItem.value="";
+//  	}
+//  	else
+//  	{
+//  		var errorMessage = document.querySelector("label");
+//  		errorMessage.innerHTML = errorString;
+//  	}
+// }
+//  addBtn.addEventListener("click",addToList)
+
+var display = document.getElementById("display");
+var buttons = document.getElementsByClassName("buttons");
+var plusButton = document.getElementById("plusbutton");
+var cancel = document.getElementById("cancelBtn");
+console.log(buttons);
+
+function addToDisplay(value)
 {
-	if (newItem.value.length > 0 )
- 	{
- 		var li = document.createElement("li");
- 		li.appendChild(document.createTextNode(newItem.value));
- 		list1.appendChild(li);
- 		newItem.value="";
- 	}
- 	else
- 	{
- 		var errorMessage = document.querySelector("label");
- 		errorMessage.innerHTML = errorString;
- 	}
+	display.value = display.value+value;
 }
- addBtn.addEventListener("click",addToList)
+
+// Displaying values on the display
+
+buttons[0].addEventListener("click",function()
+	{
+		addToDisplay(buttons[0].innerHTML);
+	}
+	);
+buttons[1].addEventListener("click",function()
+	{
+		addToDisplay(buttons[1].innerHTML);
+	}
+	);
+buttons[2].addEventListener("click",function()
+	{
+		addToDisplay(buttons[2].innerHTML);
+	}
+	);
+buttons[3].addEventListener("click",function()
+	{
+		addToDisplay(buttons[3].innerHTML);
+	}
+	);
+buttons[4].addEventListener("click",function()
+	{
+		addToDisplay(buttons[4].innerHTML);
+	}
+	);
+buttons[5].addEventListener("click",function()
+	{
+		addToDisplay(buttons[5].innerHTML);
+	}
+	);
+buttons[6].addEventListener("click",function()
+	{
+		addToDisplay(buttons[6].innerHTML);
+	}
+	);
+buttons[7].addEventListener("click",function()
+	{
+		addToDisplay(buttons[7].innerHTML);
+	}
+	);
+buttons[8].addEventListener("click",function()
+	{
+		addToDisplay(buttons[8].innerHTML);
+	}
+	);
+buttons[9].addEventListener("click",function()
+	{
+		addToDisplay(buttons[9].innerHTML);
+	}
+	);
+
+// Functionality for cancel button
+cancel.addEventListener("click",function()
+{
+	display.value = "";
+}
+);
+
+
+var string  = "sdvsd+dvsdvs+sdvsd";
+var nw = string.split("+");
+console.log(nw);
